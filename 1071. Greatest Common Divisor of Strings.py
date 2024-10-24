4c72a80b1ab829f2acb0241395cb230d671fc2b9
Jordan-Cui <140301717+Jordan-Cui@users.noreply.github.com>
2024-10-23 22:25:36 -0400

    Update 1071. Greatest Common Divisor of Strings.py

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -1,25 +1,15 @@
 class Solution:
+    def dividesString(self, string: str, divisor: str) -> bool:
+        if len(string) % len(divisor) == 0:
+            for i in range(0, len(string), len(divisor)):
+                if string[i:i+len(divisor)] != divisor:
+                    return False
+            return True
+        return False
+
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        #find divisors of length of strings
-        len1 = len(str1)
-        len2 = len(str2)
-        gcd = ""
-        for i in range(1, min(len1, len2) + 1):
-            if len1 % i == 0 and len2 % i == 0:
-                #check if they divide string
-                divides1 = True
-                divides2 = True
-                for j in range(1, len1 // i):
-                    #print(str1[0: i], " ", str1[i * j: i * (j + 1)], " \n")
-                    if str1[0: i] != str1[i * j: i * (j + 1)]:
-                        divides1 = False
-                        break
-                if divides1 and str1[0: i] == str2[0: i]:
-                    for j in range(1, len2 // i):
-                        #print(str2[0: i], " ", str2[i * j: i * (j + 1)], " \n")
-                        if str2[0: i] != str2[i * j: i * (j + 1)]:
-                            divides2 = False
-                            break
-                    if divides1 and divides2:
-                        gcd = str1[0:i]
-        return(gcd)
+        x = ""
+        for i in range(1, len(str1) + 1):
+            if self.dividesString(str1, str1[:i]) and self.dividesString(str2, str1[:i]):
+                x = str1[:i]
+        return x
